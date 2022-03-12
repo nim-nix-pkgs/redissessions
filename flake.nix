a304c5dfb6f3fd3b303620eee2cbab84057bfd84
hugosenari <hugosenari@gmail.com>
2022-03-12 02:34:25 -0300

    chore: re index redissessions

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Redis-backed sessions for jester'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."redissessions-master".url = "path:./master";
+  inputs."redissessions-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."redissessions-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
